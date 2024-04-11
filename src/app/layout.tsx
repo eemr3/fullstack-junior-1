b765f3dfe7f91cf6b37b7ad6cf4a912182aa5453
Emerson Moreira <eemr3@yahoo.com.br>
2024-04-11 20:48:59 -0300

    feat: add provider "JobProvider"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Poppins } from 'next/font/google';
 import './globals.css';
+import { JobProvider } from '../context/JobContext';
 
 const poppins = Poppins({
   subsets: ['latin'],
@@ -20,7 +21,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br">
-      <body className={poppins.className}>{children}</body>
+      <body className={poppins.className}>
+        <JobProvider>{children}</JobProvider>
+      </body>
     </html>
   );
 }
