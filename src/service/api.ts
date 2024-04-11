ed0c0a264db6bbeb6374963a04c76162aeba6abd
Emerson Moreira <eemr3@yahoo.com.br>
2024-04-11 20:47:34 -0300

    feat: create file for request API "api.ts"

---
+++ src/service/api.ts
@@ -0,0 +1,33 @@
+const SECRETE_KEY = 'naranja-labs'; // Essa chave deve ser guardada em um arquivo .env. Para fins de demonstração/didático, ela está aqui.
+
+export const getAllJobs = async () => {
+  try {
+    const result = await fetch(`http://localhost:3000/api/jobs`, {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${SECRETE_KEY}`,
+      },
+    });
+
+    return result.json();
+  } catch (error) {
+    console.error('Error:', error);
+    return error;
+  }
+};
+
+export const getJobsByLevel = async (level: string) => {
+  try {
+    const result = await fetch(`http://localhost:3000/api/jobs?level=${level}`, {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${SECRETE_KEY}`,
+      },
+    });
+
+    return result.json();
+  } catch (error) {
+    console.error('Error:', error);
+    return error;
+  }
+};
