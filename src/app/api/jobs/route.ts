12ca2804e3590a905f6f1d6aa8665b8a3316f57a
Emerson Moreira <eemr3@yahoo.com.br>
2024-04-11 20:46:42 -0300

    feat: add return find by ID in method GET "search query params"

--- src/app/api/jobs/route.ts
+++ src/app/api/jobs/route.ts
@@ -3,10 +3,20 @@ import { jobs } from '../../../database/jobs';
 
 export async function GET(request: NextRequest) {
   const secreteKey = request.headers.get('Authorization');
+  const search = new URL(request.url);
+  const level = search.searchParams;
 
   if (secreteKey?.replace('Bearer ', '') !== 'naranja-labs') {
     return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
   }
 
-  return NextResponse.json({ jobs, status: 200 });
+  if (level.get('level')) {
+    const jobFiltered = jobs.filter((job) => job.level === level.get('level'));
+    if (jobFiltered.length === 0) {
+      return NextResponse.json({ message: 'No jobs found', status: 404 });
+    }
+    return NextResponse.json({ jobs: jobFiltered, status: 200 });
+  } else {
+    return NextResponse.json({ jobs, status: 200 });
+  }
 }
