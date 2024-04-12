cccf5cdfa6d3e64804207e88a142ee33ce434ada
Emerson Moreira <eemr3@yahoo.com.br>
2024-04-12 12:33:56 -0300

    feat: add function opem Modal in Card

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,8 +1,21 @@
 'use client';
 
 import { JobsProps } from '../../common/interfaces/jobs';
+import { useJobs } from '../../context/JobContext';
 
 export function Card({ job }: JobsProps) {
+  const { setShowModal, setDataInput, setMessageSuccess } = useJobs();
+  const handleOpenModal = () => {
+    setDataInput({
+      name: '',
+      age: '',
+      phone: '',
+      city: '',
+      state: '',
+    });
+    setMessageSuccess('');
+    setShowModal(true);
+  };
   return (
     <div>
       <div className="group p-4 bg-gray-700 opacity-95  lg:p-8">
@@ -18,11 +31,12 @@ export function Card({ job }: JobsProps) {
         <div className="flex items-center justify-between">
           <button
             disabled={job.status !== 'open'}
-            className={`font-medium text-gray-100 ${
+            className={`font-medium text-gray-100 border border-yellow-500 p-2 rounded ${
               job.status === 'open'
                 ? 'transition-all duration-300 group-hover:text-gray-200/80 '
                 : 'cursor-not-allowed'
             }`}
+            onClick={handleOpenModal}
           >
             Apply now
           </button>
