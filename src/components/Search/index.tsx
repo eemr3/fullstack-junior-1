b933495266d546f9aa6bc1968d8122ee57fc982f
Emerson Moreira <eemr3@yahoo.com.br>
2024-04-11 21:57:58 -0300

    refactor: refacot component Search

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -2,6 +2,8 @@
 
 import React from 'react';
 import { useJobs } from '../../context/JobContext';
+import { InputCustom } from '../Input';
+import Lupa from '../Icons/Lupa';
 
 export default function Search() {
   const [selectTypeSearch, setSelectTypeSearch] = React.useState<string>('level');
@@ -10,58 +12,64 @@ export default function Search() {
 
   return (
     <div>
-      <label htmlFor="search">Search Job for level</label>
+      <label className="text-white" htmlFor="search">
+        Search Job for:
+      </label>
       <div className="flex gap-3 my-3">
-        <div className="flex gap-2 text-white">
-          <input
-            id="level"
-            type="radio"
-            name="level"
-            value="level"
-            checked={selectTypeSearch === 'level'}
-            onChange={() => {
-              setSearchForLevel('');
-              setSelectTypeSearch('level');
-            }}
-          />
-          <label htmlFor="level">Level</label>
-        </div>
-        <div className="flex gap-2 text-white">
-          <input
-            id="ID"
-            type="radio"
-            name="ID"
-            value="ID"
-            checked={selectTypeSearch === 'ID'}
-            onChange={() => {
-              setSearchForId('1');
-              setSelectTypeSearch('ID');
-            }}
-          />
-          <label htmlFor="ID">ID</label>
-        </div>
+        <InputCustom
+          id="level"
+          type="radio"
+          name="level"
+          value="level"
+          checked={selectTypeSearch === 'level'}
+          onChange={() => {
+            setSearchForLevel('');
+            setSearchForId('');
+            setSelectTypeSearch('level');
+          }}
+          text="Level"
+          className="h-4 w-4 border-gray-300 focus:ring-2 focus:ring-blue-300"
+        />
+        <InputCustom
+          id="ID"
+          type="radio"
+          name="ID"
+          value="ID"
+          checked={selectTypeSearch === 'ID'}
+          onChange={() => {
+            setSearchForId('1');
+            setSelectTypeSearch('ID');
+          }}
+          text="ID"
+          className="h-4 w-4 border-gray-300 focus:ring-2 focus:ring-blue-300"
+        />
       </div>
-      {selectTypeSearch === 'level' ? (
-        <input
-          type="text"
-          name="search"
-          id="search"
-          placeholder="E.g., Junior"
-          value={searchForLevel}
-          onChange={(e) => setSearchForLevel(e.target.value)}
-        />
-      ) : (
-        <input
-          type="number"
-          name="search"
-          id="search"
-          defaultValue={1}
-          min={1}
-          value={searchForId}
-          onChange={(e) => setSearchForLevel(e.target.value)}
-        />
-      )}
-      <button onClick={handleSearch}>Search</button>
+      <div className="flex gap-2 mb-10">
+        {selectTypeSearch === 'level' ? (
+          <InputCustom
+            id="search"
+            type="text"
+            name="search"
+            placeholder="E.g., Junior"
+            value={searchForLevel}
+            onChange={(e) => setSearchForLevel(e.target.value)}
+            className="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          />
+        ) : (
+          <InputCustom
+            id="searchForId"
+            type="number"
+            name="searchForId"
+            min={1}
+            value={searchForId}
+            onChange={(e) => setSearchForId(e.target.value)}
+            className="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          />
+        )}
+        <button onClick={handleSearch}>
+          <Lupa className="fill-white" />
+        </button>
+      </div>
     </div>
   );
 }
